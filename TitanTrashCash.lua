0f73b10a3bd81e1e01747188650eff9d9f3f616e
André Evertz <andre.evertz@devil-coding.de>
2017-08-29 21:11:15 +0200

    Added counting function

--- TitanTrashCash.lua
+++ TitanTrashCash.lua
@@ -57,15 +57,61 @@ end
 
 -- **************************************************************************
 -- NAME : TitanTrashCash_GetButtonText()
--- DESC : Calculate the item count of the tracked farm item and displays it.
+-- DESC : Calculate the money amount of trash items.
 -- **************************************************************************
 function TitanTrashCash_GetButtonText(id)
 
-	local str = 'test';
+  local trashCount = 0;
+  local trashAmount = 0;
 
-	return str;
+  for bag = 0, 5 do
+    for slot = 1, GetContainerNumSlots(bag) do
+      local count, _, quality, _, _, _, _, _, itemID = select(2, GetContainerItemInfo(bag, slot));
+      if itemID ~= nil and quality == 0 then
+        local itemSellPrice = select(11, GetItemInfo(itemID));
+        trashCount = trashCount + 1;
+        trashAmount = trashAmount + (count * tonumber(select(11, GetItemInfo(itemID))));
+      end
+    end
+  end
+
+	return TitanTrashCash:FormatMoney(trashAmount);
 end
 
+function TitanTrashCash:FormatMoney(amount)
+
+  local str = '';
+  local oneGold = 10000;
+  local oneSilver = 100;
+
+  gold = (math.floor(amount / oneGold) or 0)
+  amount = amount - (gold * oneGold);
+  silver = (math.floor(amount / oneSilver) or 0)
+  copper = amount - (silver * oneSilver)
+
+  if gold > 0 then
+    str = TitanTrashCash:GetIconString('Interface\\MoneyFrame\\UI-GoldIcon', true) .. tostring(gold) .. ' ';
+    str = str .. TitanTrashCash:GetIconString('Interface\\MoneyFrame\\UI-SilverIcon', true) .. tostring(silver) .. ' ';
+  elseif silver > 0 then
+    str = str .. TitanTrashCash:GetIconString('Interface\\MoneyFrame\\UI-SilverIcon', true) .. tostring(silver) .. ' ';
+  end
+  str = str .. TitanTrashCash:GetIconString('Interface\\MoneyFrame\\UI-CopperIcon', true) .. tostring(copper);
+
+  return str;
+end
+
+-- **************************************************************************
+-- NAME : TitanTrashCash:GetIconString()
+-- DESC : Gets an icon string.
+-- **************************************************************************
+function TitanTrashCash:GetIconString(icon, space)
+  local fontSize = TitanPanelGetVar('FontSize');
+	local str = '|T' .. icon .. ':' .. fontSize .. ':' .. fontSize .. ':2:0|t';
+	if space == true then
+		str = str .. ' ';
+	end
+	return str;
+end
 
 
 -- **************************************************************************
