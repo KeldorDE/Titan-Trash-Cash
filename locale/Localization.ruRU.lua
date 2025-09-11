1724734187500a48ab1f6863362d0b8b0c268117
ZamestoTV <34053496+Hubbotu@users.noreply.github.com>
2025-09-11 07:00:10 +0300

    Update Localization.ruRU.lua

--- locale/Localization.ruRU.lua
+++ locale/Localization.ruRU.lua
@@ -1,20 +1,12 @@
 local L = LibStub('AceLocale-3.0'):NewLocale('Titan', 'ruRU', false)
 if not L then return end
-
+-- Translator ZamestoTV
 L = L or {}
---Translation missing 
-L["TRASH_CASH_AMOUNT"] = "Amount"
---Translation missing 
-L["TRASH_CASH_ITEM"] = "Item"
---Translation missing 
-L["TRASH_CASH_ITEMS"] = "Items"
---Translation missing 
-L["TRASH_CASH_NO_TRASH"] = "No trash was found in your bags!"
---Translation missing 
-L["TRASH_CASH_SHOW_TOP_ITEM"] = "Show top item"
---Translation missing 
-L["TRASH_CASH_TOP_ITEM"] = "Top item"
---Translation missing 
-L["TRASH_CASH_TOTAL"] = "Total"
---Translation missing 
-L["TRASH_CASH_TRASH"] = "Trash"
+L["TRASH_CASH_AMOUNT"] = "Количество"
+L["TRASH_CASH_ITEM"] = "Предмет"
+L["TRASH_CASH_ITEMS"] = "Предметы"
+L["TRASH_CASH_NO_TRASH"] = "Мусор в ваших сумках не найден!"
+L["TRASH_CASH_SHOW_TOP_ITEM"] = "Показывать основной предмет"
+L["TRASH_CASH_TOP_ITEM"] = "Основной предмет"
+L["TRASH_CASH_TOTAL"] = "Всего"
+L["TRASH_CASH_TRASH"] = "Мусор"
